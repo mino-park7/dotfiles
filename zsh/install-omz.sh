b8d33212d50599251c137d4fa6aede87f196691a
mino-park7 <minho.park2115@gmail.com>
2026-03-21 18:54:55 +0900

    Fix install-omz.sh for non-zsh environments and idempotent runs
    - Remove source oh-my-zsh.sh (fails in bash, only ZSH_CUSTOM needed)
    - Remove early exit so p10k/fzf install runs even if omz exists
    - Add fzf directory check to prevent clone failure on re-runs

--- zsh/install-omz.sh
+++ zsh/install-omz.sh
@@ -4,15 +4,11 @@ set -e
 # Check if oh-my-zsh is already installed
 if [ -d ~/.oh-my-zsh ]; then
     echo "oh-my-zsh is already installed. Skipping installation."
-    exit 0
 else
     echo "oh-my-zsh is not installed. Installing..."
     sh 0>/dev/null -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
-source ~/.oh-my-zsh/oh-my-zsh.sh
-if [[ -z "${ZSH_CUSTOM}" ]]; then
-    ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
-fi
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
 # Install powerlevel10k and configure it.
 if [ ! -d "${ZSH_CUSTOM}"/themes/powerlevel10k ]; then
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM}"/themes/powerlevel10k
@@ -21,7 +17,9 @@ fi
 sed -i -e '/source \$ZSH\/oh-my-zsh.sh/i export ZSH_COMPDUMP=\$ZSH\/cache\/.zcompdump-\$HOST' ${HOME}/.zshrc
 
 # Install fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --all
+if [ ! -d ~/.fzf ]; then
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install --all
+fi
 
 echo "oh-my-zsh installation and configuration completed."
